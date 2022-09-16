a50ac4df97286e269cceeb2e278f7ec592dbd05b
pauleentalay <pauleen.talay@cloudreach.com>
2022-09-16 12:26:46 +0200

    fix: dynamodb_lock

--- backend.tf
+++ backend.tf
@@ -2,5 +2,6 @@ terraform {
   backend "s3" {
     bucket = "ta-terraform-tfstates-247548036690"
     key    = "sprint1/week2/training-terraform/triple_subnet.tfstates"
+    dynamodb_table = "terraform-lock"
   }
 }
